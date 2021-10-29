2d7b338e6c14f899eced4771c38830dfbd9e79fa
nojvspine <92740368+nojvspine@users.noreply.github.com>
2021-10-29 11:44:45 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -177,7 +177,7 @@ def get_text_messages(message):
         elif message.text[:6] == "рп ник" or message.text[:6] == "Рп ник":
             nicks.nicks[message.from_user.first_name]=message.text[7:]
             bot.send_message(message.chat.id, "%s теперь имеет ник %s" % (message.from_user.first_name, nicks.nicks[message.from_user.first_name]))
-        elif message.text.lower() == "рп ники":
+        elif message.text.lower() == "ники рп":
             bot.send_message(message.chat.id, "Вот ники участников чата: ", nicks.nicks)
 
 bot.polling(none_stop=True, interval=0)
