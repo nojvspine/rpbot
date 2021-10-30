b32c94168d44af776f383620cde9d4e4cebe100d
nojvspine <92740368+nojvspine@users.noreply.github.com>
2021-10-30 16:37:57 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import nicks
 from telebot.types import ChatMemberMember, User;
 from telebot import types;
 import random
-bot = telebot.TeleBot('');
+bot = telebot.TeleBot('2071410162:AAEBi0TeppPrzRA8vanFyCCv_V1J7VrK6hE');
 
 def chatnick(message):
     for key in nicks.nicks.keys():
@@ -195,35 +195,5 @@ def get_text_messages(message):
         elif message.text.lower() == "ники рп":
             chatnick(message)
             bot.send_message(message.chat.id, "Вот ники участников чата:\n"+("\n".join([f'{i}: {nicks.nicks[message.chat.id][i]}' for i in nicks.nicks[message.chat.id].keys()])))
-
-@bot.message_handler(content_types=['text'])
-def brak(message):
-    if message.text.lower() == "рп брак":
-        nick(message)
-        love = (message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id)
-        lover = (message.from_user.first_name, message.from_user.id)
-        nicks.brak=(love, lover)
-        markup = telebot.types.InlineKeyboardMarkup()
-        markup.add(telebot.types.InlineKeyboardButton(text='Да', callback_data=True))
-        markup.add(telebot.types.InlineKeyboardButton(text='Нет', callback_data=False))
-        bot.send_message(message.chat.id, "[%s](tg://user?id=%s), минуточку внимания. [%s](tg://user?id=%s) сделал вам предложение руки и сердца. Нажмите да/нет чтобы принять/отказаться от предложения." % (love[0], love[1], lover[0], love[1]), parse_mode="Markdown")
-            
-@bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
-    tm=time()
-    while tm+100>time():
-        if call.data == '1':
-            brak = (nicks.nicks[call.message.chat.id][nicks.brak[1][0]], nicks.nicks[call.message.chat.id][nicks.brak[0][0]])
-            nicks.braki.append(brak)
-            bot.send_message(call.message.chat.id, "Теперь %s и %s состоят в счастливом браке!" % (nicks.nicks[call.message.chat.id][nicks.brak[1][0]], nicks.nicks[call.message.chat.id][nicks.brak[0][0]]))
-            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-            break
-        elif call.data == '0':
-            bot.send_message(call.message.chat.id, "[%s](tg://user?id=%s), сожалеем, но ваши чувства не были взаимны." % (nicks.brak[1][0], nicks.brak[1][1]), parse_mode="Markdown")
-            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-            break
-    else:
-        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-        bot.send_message(call.message.chat.id, "К сожалению, время ответа истекло. Попробуйте снова.")
 
 bot.polling(none_stop=True, interval=0)
